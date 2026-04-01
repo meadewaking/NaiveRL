9cbab766a4c50662a89f9dd02704d1332b70d7a4
meadewaking <253121341@qq.com>
2026-04-01 18:30:03 +0800

    Add algorithm/PPO-refine/config.py

---
+++ algorithm/PPO-refine/config.py
@@ -0,0 +1,14 @@
+config = {
+    'env_name': 'CartPole-v1',
+    'env_dim': 4,
+    'act_dim': 2,
+    'max_episode': 500,
+    'gamma': 0.99,
+    'lambda': 0.95,
+    'train_loop': 3,
+    'learning_rate': 3e-4,
+    'epsilon_clip': 0.2,
+    'entropy_coeff': 0.01,
+    'vf_loss_coeff': 0.5,
+    'horizon': 128,
+}
